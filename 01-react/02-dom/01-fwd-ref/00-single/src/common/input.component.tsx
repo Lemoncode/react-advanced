5ba1c0ce3fb013c848608d7e54e5a77fe2c22540
Braulio Diez Botella <braulio.diez@gmail.com>
2022-09-15 20:38:48 +0200

    single prop forward ref

---
+++ 01-react/02-dom/01-fwd-ref/00-single/src/common/input.component.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+interface InputProps {
+  label: string;
+  value: string;
+  onChange: (newValue: string) => void;
+}
+
+export const InputComponent = React.forwardRef<HTMLInputElement, InputProps>(
+  (props, ref) => {
+    const { label, value, onChange } = props;
+
+    const handleChange = (event: any) => {
+      onChange(event.target.value);
+    };
+
+    return (
+      <input
+        ref={ref}
+        placeholder={label}
+        value={value}
+        onChange={handleChange}
+      />
+    );
+  }
+);
