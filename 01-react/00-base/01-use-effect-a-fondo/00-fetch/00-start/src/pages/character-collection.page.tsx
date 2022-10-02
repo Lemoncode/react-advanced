d76e77423c83a01d77bc7d397a30703fe3f45faf
Braulio Diez Botella <braulio.diez@gmail.com>
2022-10-02 18:35:48 +0200

    collection basics

--- 01-react/00-base/01-use-effect-a-fondo/00-fetch/00-start/src/pages/character-collection.page.tsx
+++ 01-react/00-base/01-use-effect-a-fondo/00-fetch/00-start/src/pages/character-collection.page.tsx
@@ -2,10 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const CharacterCollectionPage = () => {
+  const [filter, setFilter] = React.useState("");
+  const [characters, setCharacters] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/?name=${filter}`)
+      .then((response) => response.json())
+      .then((json) => setCharacters(json.results));
+  });
+
   return (
     <>
       <h1>Character Collection</h1>
       <Link to="/1">Character 1</Link>
+      <ul>
+        {characters.map((character) => (
+          <li key={character.id}>
+            <Link to={`/${character.id}`}>{character.name}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
