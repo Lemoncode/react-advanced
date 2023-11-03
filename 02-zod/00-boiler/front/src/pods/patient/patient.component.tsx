6026fa38d687e149645114111ce41307272dd86a
manudous <manudous@gmail.com>
2023-11-03 17:25:06 +0100

    add patient form component

--- 02-zod/00-boiler/front/src/pods/patient/patient.component.tsx
+++ 02-zod/00-boiler/front/src/pods/patient/patient.component.tsx
@@ -1,12 +1,45 @@
 import React from "react";
+import { IconButton, Typography } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { useNavigate } from "react-router-dom";
+import { switchRoutes } from "@/core/router";
+import { PatientFormComponent } from "./components";
 import * as viewModel from "./patient.vm";
-
+import * as classes from "./patient.styles";
 interface Props {
   patient: viewModel.Patient;
+  onChange: (
+    fieldId: string
+  ) => (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const Patient: React.FC<Props> = (props) => {
-  const { patient } = props;
-  console.log(patient);
-  return <div>Patient: React.</div>;
+  const { patient, onChange } = props;
+  const navigate = useNavigate();
+
+  return (
+    <div className={classes.root}>
+      <header>
+        <Typography className={classes.title} variant="h1" component={"h1"}>
+          Paciente
+        </Typography>
+      </header>
+
+      <section className={classes.textFieldsContainer}>
+        <PatientFormComponent patient={patient} onChange={onChange} />
+      </section>
+
+      <IconButton
+        className={classes.goBack}
+        onClick={() => navigate(switchRoutes.patientList)}
+        aria-label="Volver al listado de edición de libros"
+        size="large"
+      >
+        <ArrowBackIcon />
+        <Typography variant="caption" component={"span"}>
+          Regresar
+        </Typography>
+      </IconButton>
+    </div>
+  );
 };
