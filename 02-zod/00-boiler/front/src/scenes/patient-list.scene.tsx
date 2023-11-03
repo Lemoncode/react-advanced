58b856ac56d2424f7ae16991ca45f3b5644e46e7
manudous <manudous@gmail.com>
2023-11-03 13:20:25 +0100

    create scenes

---
+++ 02-zod/00-boiler/front/src/scenes/patient-list.scene.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PatientListScene: React.FC = () => {
+  return <h1>Hola desde Book Scene</h1>;
+};
---
+++ 02-zod/00-boiler/front/src/scenes/patient.scene.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PatientScene: React.FC = () => {
+  return <h1>PatientScene</h1>;
+};
