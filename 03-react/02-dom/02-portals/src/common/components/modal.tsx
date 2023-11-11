dc6e90ff304ed66c552325d21f39508f39d8fc77
Braulio Diez Botella <braulio.diez@gmail.com>
2023-11-11 15:19:08 +0100

    completed basic modal example

---
+++ 03-react/02-dom/02-portals/src/common/components/modal.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import classes from "./modal.module.css";
+
+interface Props {
+  children: React.ReactNode;
+  isOpen: boolean;
+  handleClose: () => void;
+}
+
+export const Modal: React.FC<Props> = (props) => {
+  const { children, isOpen, handleClose } = props;
+
+  React.useEffect(() => {
+    const closeOnEscapeKey = (e: KeyboardEvent) =>
+      e.key === "Escape" ? handleClose() : null;
+    document.body.addEventListener("keydown", closeOnEscapeKey);
+    return () => {
+      document.body.removeEventListener("keydown", closeOnEscapeKey);
+    };
+  }, [handleClose]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className={classes.modal}>
+      <button className={classes.closeBtn} onClick={handleClose}>
+        Close
+      </button>
+      <div className={classes.modalContent}>{children}</div>
+    </div>
+  );
+};
