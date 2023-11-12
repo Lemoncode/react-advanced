e0bda4dff22fbbc6959d65bee4de2a6318562a0e
Braulio Diez Botella <braulio.diez@gmail.com>
2023-11-12 14:24:56 +0100

    drop card start

---
+++ 03-react/02-dom/03-drag-drop-reducer/01-drop-card/src/kanban/providers/kanban.provider.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { moveCardColumn } from "../kanban.business";
+import {
+  KanbanContent,
+  createDefaultKanbanContent,
+  DragItemInfo,
+} from "../model";
+import { KanbanContext } from "./kanban.context";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const KanbanProvider: React.FC<Props> = ({ children }) => {
+  const [kanbanContent, setKanbanContent] = React.useState<KanbanContent>(
+    createDefaultKanbanContent()
+  );
+
+  const moveCard = (
+    columnDestinationId: number,
+    index: number,
+    dragItemInfo: DragItemInfo
+  ) => {
+    const { columnId: columnOriginId, content } = dragItemInfo;
+
+    setKanbanContent((kanbanContentLatest) =>
+      moveCardColumn(
+        {
+          columnOriginId,
+          columnDestinationId,
+          cardIndex: index,
+          content,
+        },
+        kanbanContentLatest
+      )
+    );
+  };
+
+  return (
+    <KanbanContext.Provider
+      value={{
+        kanbanContent,
+        setKanbanContent,
+        moveCard,
+      }}
+    >
+      {children}
+    </KanbanContext.Provider>
+  );
+};
