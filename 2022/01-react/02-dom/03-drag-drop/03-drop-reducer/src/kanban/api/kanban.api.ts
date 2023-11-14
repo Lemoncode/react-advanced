b1e5a685b1f1233c18e0f77640964041e44d5709
Braulio Diez Botella <braulio.diez@gmail.com>
2023-11-14 21:18:20 +0100

    remove 2022 drop

--- 2022/01-react/02-dom/03-drag-drop/03-drop-reducer/src/kanban/api/kanban.api.ts
+++
@@ -1,6 +0,0 @@
-import { CardContent, KanbanState } from "../model";
-import { mockData } from "./mock-data";
-
-export const loadKanbanContent = async (): Promise<KanbanState> => {
-  return mockData;
-};
--- 2022/01-react/02-dom/03-drag-drop/03-drop-reducer/src/kanban/providers/kanban.context.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { KanbanAction, KanbanState } from "../model";
-
-export interface KanbanContextModel {
-  kanbanContent: KanbanState;
-  dispatch: React.Dispatch<KanbanAction>;
-}
-
-export const KanbanContext = React.createContext<KanbanContextModel>(null);
