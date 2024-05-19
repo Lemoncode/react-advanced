fb416d11d89b7d5dd89ec6fb6ad3172c617f0cb6
Braulio <braulio.diez@gmail.com>
2024-05-19 21:05:00 +0200

    react query example

---
+++ 03-react/01-base/01-use-effect-a-fondo/02-react-query/src/pages/character-detail/character-detail.page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { getCharacter } from "./character-detail.api";
+import { Character } from "./character-detail.model";
+
+export const CharacterDetailPage = () => {
+  const { characterId } = useParams();
+
+  // Aplicamos alias a _character_ a _data_
+  const { data: character } = useQuery({
+    queryKey: ["character", characterId],
+    queryFn: () => getCharacter(characterId ?? ""),
+  });
+
+  return (
+    <>
+      <h1>Character Page</h1>
+      <Link to="/">Back to Character Collection</Link>
+      {character && (
+        <div>
+          <h2>{character.name}</h2>
+          <img src={character.image} alt={character.name} />
+          <h4>{character.gender}</h4>
+          <h4>{character.status}</h4>
+          <h4>{character.type}</h4>
+        </div>
+      )}
+    </>
+  );
+};
