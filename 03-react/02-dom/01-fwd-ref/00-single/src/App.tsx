02b18aa95e16d29a4d5784f8e66184d2816983e3
Braulio <braulio.diez@gmail.com>
2024-05-26 13:30:36 +0200

    single completed

---
+++ 03-react/02-dom/01-fwd-ref/00-single/src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { InputComponent } from "./common/input.component";
+
+export default function App() {
+  const secondInputRef = React.useRef<HTMLInputElement>(null);
+  const [firstName, setFirstName] = React.useState("");
+  const [secondName, setSecondName] = React.useState("");
+
+  const handleSetFocus = () => {
+    if (secondInputRef.current) {
+      secondInputRef.current.focus();
+    }
+  };
+
+  return (
+    <div className="App">
+      <InputComponent
+        label="First name"
+        value={firstName}
+        onChange={setFirstName}
+      />
+      <InputComponent
+        ref={secondInputRef}
+        label="Second name"
+        value={secondName}
+        onChange={setSecondName}
+      />
+      <button onClick={handleSetFocus}>Set focus to second name</button>
+    </div>
+  );
+}
